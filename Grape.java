03021f31f617cdbee3cb9e07ec7b589e1deae1d2
shaky008 <sshakya.uutsav@gmail.com>
2021-10-14 09:29:16 +0545

    Added getter and setter method for weight in Grape.java

--- Grape.java
+++ Grape.java
@@ -3,6 +3,15 @@ public class Grape {
     public String toString() {
         return "Founder of Grape: Utsav Shakya";
     }
+    //setter
+    public void setWeight (float weightFruit) {
+        this.weight = weightFruit;
+    }
+    //getter
+    public float getWeight() {
+        return weight;
+    }
+
 }
 
 
