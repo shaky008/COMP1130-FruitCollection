7ada00240f482ebea24ba3dc4b17c174059b8e2f
shaky008 <sshakya.uutsav@gmail.com>
2021-10-14 23:34:17 +0545

    OrangeDriver

---
+++ OrangeDriver.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class OrangeDriver {
+
+    public static void main(String[] args) {
+        Orange fruit = new Orange ();
+        System.out.println(fruit);
+
+    }
+
+}
