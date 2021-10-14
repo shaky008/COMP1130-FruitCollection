a80f741456cc23c5050d61f68b29a3d8ea899081
shaky008 <sshakya.uutsav@gmail.com>
2021-10-14 23:33:59 +0545

    added getter and setter

---
+++ Orange.java
@@ -0,0 +1,16 @@
+package com.company;
+public class Orange {
+    private float weight;
+
+    public String toString() {
+            return " This is an Orange created by Alina ";
+    }
+        //setter
+    public void setWeight (float weight2) {
+        this.weight = weight2;
+    }
+    //getter
+    public float getWeight() {
+        return weight;
+        }
+    }
