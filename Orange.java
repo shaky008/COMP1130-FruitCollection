7a5657337315ec5937877d4a89f89f0f99038252
Alina Rattanshi <90849551+alinaratttanshi@users.noreply.github.com>
2021-10-16 14:18:08 +0300

    getters and setters

--- Orange.java
+++ Orange.java
@@ -1,9 +1,18 @@
 package comp1130;
 
 public class Orange {
+	 private float weight;
 
-	public String toString() {
-	
-		return " This is an Orange created by Alina ";
-	}
- }
+	    public String toString() {
+	            return " This is an Orange created by Alina ";
+	    }
+	        //setter
+	    public void setWeight (float weight2) {
+	        this.weight = weight2;
+	    }
+	    //getter
+	    public float getWeight() {
+	        return weight;
+	        }
+	    }
+ 
